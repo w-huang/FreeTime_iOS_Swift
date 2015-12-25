02e42cdc1cf4022e320e6df3a7af69ac3a4620c5
Wilson <Wilson@Wilsons-MacBook-Pro.local>
2015-12-24 20:57:03 -0800

    first commit

---
+++ NoProcrastination/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NoProcrastination
+//
+//  Created by Wilson on 2015-12-24.
+//  Copyright © 2015 Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
